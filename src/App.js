80322946e0d58462674e6aca582ddceec2b558fd
rrayatec <79470126+rrayatec@users.noreply.github.com>
2021-08-31 18:23:54 -0500

    router basic

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import {
   Link
 } from "react-router-dom";
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 export default function BasicExample() {
   return (
     <Router>
@@ -42,9 +33,6 @@ export default function BasicExample() {
     </Router>
   );
 }
-
-// You can think of these components as "pages"
-// in your app.
 
 function Home() {
   return (
