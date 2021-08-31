dd9723f1250b207bd81b39f59260268458a82714
rrayatec <79470126+rrayatec@users.noreply.github.com>
2021-08-31 17:22:00 -0500

    new init

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@
 import "./App.css";
 import "./index.css";
 import React, { useState, useContext } from "react";
-import NewInput from "./components/NewInput";
 
 function App() {
   
-  const [count, setCount] = useState(10);
-  const [dato, setDato] = useState("Sergio");
   const [users, setUsers] = useState([{ id: 0, name: "Ruben" }]);
 
-  const onClick = () => {
-    setUsers([...users, { id: users.length, name: "Sergio" }]);
-  };
-
-
-  // var datos = {
-  //   type: "button",
-  //   value: "Insertar",
-  // };
-  
+  const onClick = () => {};  
 
   return (
     <div>
