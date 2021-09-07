ecfc98fe9e5ba05a20fe701fd9a1bcdf5df0c61f
rrayatec <79470126+rrayatec@users.noreply.github.com>
2021-09-07 18:23:27 -0500

    new bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Button,
+  Modal,
+  Offcanvas,
+} from "react-bootstrap";
 
+import { Bus, Cart } from "./routes/TacosRoute";
 // Some folks find value in a centralized route config.
 // A route config is just data. React is great at mapping
 // data into components, and <Route> is a component.
@@ -16,7 +21,7 @@ import {
 const routes = [
   {
     path: "/sandwiches",
-    component: Sandwiches
+    component: Sandwiches,
   },
   {
     path: "/tacos",
@@ -24,35 +29,95 @@ const routes = [
     routes: [
       {
         path: "/tacos/bus",
-        component: Bus
+        component: Bus,
       },
       {
         path: "/tacos/cart",
-        component: Cart
-      }
-    ]
-  }
+        component: Cart,
+      },
+    ],
+  },
 ];
 
 export default function RouteConfigExample() {
+  const [modalShow, setModalShow] = useState(false);
+  console.log(modalShow);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleSelect = () => {
+    setModalShow(true);
+  };
+
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/tacos">Tacos</Link>
-          </li>
-          <li>
-            <Link to="/sandwiches">Sandwiches</Link>
-          </li>
-        </ul>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">
+          <img
+            alt=""
+            src="https://react-bootstrap.github.io/logo.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          SAI
+        </Navbar.Brand>
 
+        <Nav defaultActiveKey="/">
+          <Nav.Item>
+            <Nav.Link eventKey="Tacos">
+              <Link to="/tacos">Tacos</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="Sandwiches">
+              <Link to="/sandwiches">Sandwiches</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+
+        <Navbar.Collapse className="justify-content-end">
+          <NavDropdown title={"Ruben"} id="collasible-nav-dropdown">
+            <NavDropdown.Item>Home</NavDropdown.Item>
+            <NavDropdown.Item>Another action</NavDropdown.Item>
+            <NavDropdown.Item>Something</NavDropdown.Item>
+            <NavDropdown.Item onClick={handleShow}> Launch </NavDropdown.Item>
+            <NavDropdown.Item onClick={() => handleSelect(true)}>
+              Info
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+
+      <Offcanvas show={show} onHide={handleClose} placement={"bottom"}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+      <Container>
         <Switch>
           {routes.map((route, i) => (
             <RouteWithSubRoutes key={i} {...route} />
           ))}
         </Switch>
-      </div>
+      </Container>
     </Router>
   );
 }
@@ -64,7 +129,7 @@ function RouteWithSubRoutes(route) {
   return (
     <Route
       path={route.path}
-      render={props => (
+      render={(props) => (
         // pass the sub-routes down to keep nesting
         <route.component {...props} routes={route.routes} />
       )}
@@ -98,10 +163,32 @@ function Tacos({ routes }) {
   );
 }
 
-function Bus() {
-  return <h3>Bus</h3>;
-}
-
-function Cart() {
-  return <h3>Cart</h3>;
+function MyVerticallyCenteredModal(props) {
+  console.log(props);
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      {console.log(props)}
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
---
+++ src/routes/TacosRoute.jsx
@@ -0,0 +1,10 @@
+function Bus() {
+    return <h3>Bus</h3>;
+  }
+  
+  function Cart() {
+    return <h3>Cart</h3>;
+  }
+
+
+  export {Bus, Cart}
