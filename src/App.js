71164b77b27344349ec83daff4ff4a5b824e91fb
rrayatec <79470126+rrayatec@users.noreply.github.com>
2021-09-07 20:09:53 -0500

    new bootstrap no render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  Container,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Button,
+  Modal,
+  Offcanvas,
+} from "react-bootstrap";
 
-export default function RouteConfigExample() {
+function App() {
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/tacos">Tacos</Link>
-          </li>
-          <li>
-            <Link to="/sandwiches">Sandwiches</Link>
-          </li>
-        </ul>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="https://react-bootstrap.github.io/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            SAI
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
 
-        <Switch>
-          <Route path={"/tacos"} component={Tacos} />
-          <Route path={"/sandwiches"} component={Sandwiches} />
-        </Switch>
-      </div>
+      <Link to="/tacos">Tacos</Link>
+      <Link to="/sandwiches">Sandwiches</Link>
+
+      <Switch>
+        <Route path={"/tacos"} component={Tacos} />
+        <Route path={"/sandwiches"} component={Sandwiches} />
+      </Switch>
     </Router>
   );
 }
@@ -27,6 +43,8 @@ function Sandwiches() {
   return <h2>Sandwiches</h2>;
 }
 
-function Tacos() {
+function Tacos(data) {
   return <h2>Tacos</h2>;
 }
+
+export default App;
